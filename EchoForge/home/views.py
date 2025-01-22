a7d73a4087147ccf0838d7e21e554021da918ac4
Lakios_ <quentin.gehin@epitech.eu>
2025-01-22 20:22:31 +0100

    login mod

--- EchoForge/home/views.py
+++ EchoForge/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
@@ -8,27 +8,27 @@ from home.games.sokoban import *
 from .forms import DirectionForm, ResetForm
 
 def logout(request):
-    logout(request)
+    auth_logout(request)  # Utilise le module de logout
     return redirect('login')
 
-def login(request):
+def login_view(request):  # Renommé pour éviter le conflit
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
-            return redirect('home')
+            auth_login(request, user)  # Utilise le module login correctement
+            return redirect('home')  # Redirection vers 'home' après connexion réussie
         else:
             return HttpResponse('Invalid login', status=401)
-    return redirect('login')
+    return render(request, 'login.html')  # Retourne la page de login si ce n'est pas une requête POST
 
 @login_required
 def home(request):
     return render(request, 'home.html')
 
 def combine_box(request):
-    cb_board = request.session.get('cb_board', [[1] * 4 for _ in range (4)])
+    cb_board = request.session.get('cb_board', [[1] * 4 for _ in range(4)])
     cb_score = request.session.get('cb_score', 0)
     if request.method == 'POST':
         resetForm = ResetForm(request.POST)
@@ -46,7 +46,7 @@ def combine_box(request):
     return render(request, 'games/combine_box.html', context)
 
 def sokoban(request):
-    sokoban_board = request.session.get('sokoban_board', [['#'] * 10 for _ in range (10)])
+    sokoban_board = request.session.get('sokoban_board', [['#'] * 10 for _ in range(10)])
     if request.method == 'POST':
         resetForm = ResetForm(request.POST)
         directionForm = DirectionForm(request.POST)
